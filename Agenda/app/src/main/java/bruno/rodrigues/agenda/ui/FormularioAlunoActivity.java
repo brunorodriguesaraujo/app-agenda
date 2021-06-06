8cd889810b97dc9a1724276e6e3232bc4f18427c
Bruno Rodrigues <brunorodrigues-78@hotmail.com>
2021-06-06 18:39:18 -0300

    criando a classe aluno e adicionando funcionalidades na activity

--- Agenda/app/src/main/java/bruno/rodrigues/agenda/ui/FormularioAlunoActivity.java
+++ Agenda/app/src/main/java/bruno/rodrigues/agenda/ui/FormularioAlunoActivity.java
@@ -3,8 +3,13 @@ package bruno.rodrigues.agenda.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import bruno.rodrigues.agenda.R;
+import bruno.rodrigues.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +17,24 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final EditText campoNome = findViewById(R.id.et_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.et_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.et_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno aluno = new Aluno(nome, telefone, email);
+
+                
+
+            }
+        });
     }
 }
